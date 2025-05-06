01a6715489740fcc192856435a7675fb1b531715
Dmytro Savchuk <dmytro.savchuk.ua@gmail.com>
2025-05-06 17:59:41 +0300

    init

--- KPZ_MKR_1/Program.cs
+++ KPZ_MKR_1/Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+﻿using System.Text;
+
 
 class Program
 {
